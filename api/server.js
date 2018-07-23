29d9af317d7b09c9bff91c412c690c288e28af85
Anderson Abreu do Nascimento <anderson.abreu@prodam.am.gov.br>
2018-07-23 16:02:56 -0400

    Atualização do código

--- api/server.js
+++ api/server.js
@@ -1,56 +1,111 @@
-let express = require('express'),
-	bodyParser = require('body-parser'),
-	mongodb = require('mongodb'),
-	objectId = require('mongodb').ObjectId;
+'use strict';
 
-let app = express();
+const express = require('express'),
+	bodyParser = require('body-parser'),
+	multiparty = require('connect-multiparty'),
+	mongodb = require('mongodb'),
+	objectId = require('mongodb').ObjectId,
+	mv =  require('mv'),
+	fs =  require('fs');
+
+const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(multiparty());
 
-let port = 8080;
+const port = 8080;
 
 app.listen(port);
 
 console.log('Servidor HTTP esta escutando na porta: ' + port);
 
-let db = new mongodb.Db(
+const db = new mongodb.Db(
 	'instagram',
 	new mongodb.Server('192.168.33.10', 27017, {}),
 	{}
 );
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
 
-	res.send({msg: 'Olá'})
+	res.send({msg: 'Olá'});
 
 });
 
-app.post('/api', function (req, res) {
+app.get('/imagens/:imagem', (req, res) => {
+	let img = req.params.imagem;
 
-	let dados = req.body;
+	fs.readFile('./uploads/'+img, (err, content) => {
+		if (err) {
+			res.status(400).json(err);
+			return;
+		}
+		res.writeHead(200, {'Content-type': 'image/jpg'});
+		res.end(content);
+	});
+});
 
-	db.open(function(err, mongoClient){
-		mongoClient.collection('postagens', function(err, collection) {
-			collection.insert(dados, function(err, records) {
-				if(err) {
-					res.json({status: 'erro'});
-				} else {
-					res.json({status: 'inclusão realizada com sucesso'});
+app.post('/api', (req, res) => {
+
+	res.setHeader("Access-Control-Allow-Origin", "*");
+
+	let time_stamp = new Date().getTime();
+	let url_imagem = time_stamp + '_' + req.files.arquivo.originalFilename;
+
+	// res.send(req.body);
+	let path_origem = req.files.arquivo.path;
+	let path_destino = './uploads/' + url_imagem;
+
+	const dados = {
+		url_imagem: url_imagem,
+		titulo: req.body.titulo
+	};
+
+	mv(path_origem, path_destino, (err) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+
+		db.open((err, mongoClient) => {
+			if (err) {
+				res.status(500).json({status: 'erro', msg: 'Desculpe! Houve um erro interno na aplicação.'});
+				return;
+			}
+			mongoClient.collection('postagens', (err, postagens) => {
+				if (err) {
+					res.status(500).json({status: 'erro', msg: 'Não foi possivel acessar a collection (postagens)'});
+					return;
 				}
+				postagens.insert(dados, (err, records) => {
+					if (err) {
+						res.json({status: 'erro'});
+					} else {
+						res.json({status: 'inclusão realizada com sucesso'});
+					}
+				});
+				mongoClient.close();
 			});
-			mongoClient.close();
 		});
 	});
-
+		
 });
 
-app.get('/api', function (req, res) {
+app.get('/api', (req, res) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
 
-	db.open(function(err, mongoClient){
-		mongoClient.collection('postagens', function(err, collection) {
-			collection.find().toArray(function(err, results) {
-				if(err) {
+	db.open((err, mongoClient) => {
+		if (err) {
+			res.status(500).json({status: 'erro', msg: 'Desculpe! Houve um erro interno na aplicação.'});
+			return;
+		}
+		mongoClient.collection('postagens', (err, postagens) => {
+			if (err) {
+				res.status(500).json({status: 'erro', msg: 'Não foi possivel acessar a collection (postagens)'});
+				return;
+			}
+			postagens.find().toArray((err, results) => {
+				if (err) {
 					res.json(err);
 				} else {
 					res.json(results);
@@ -62,12 +117,12 @@ app.get('/api', function (req, res) {
 
 });
 
-app.get('/api/:id', function (req, res) {
+app.get('/api/:id', (req, res) => {
 
-	db.open(function(err, mongoClient){
-		mongoClient.collection('postagens', function(err, collection) {
-			collection.find(objectId(req.params.id)).toArray(function(err, results) {
-				if(err) {
+	db.open((err, mongoClient) => {
+		mongoClient.collection('postagens', (err, postagens) => {
+			postagens.find(objectId(req.params.id)).toArray((err, results) => {
+				if (err) {
 					res.json(err);
 				} else {
 					res.json(results);
@@ -79,16 +134,17 @@ app.get('/api/:id', function (req, res) {
 
 });
 
-app.put('/api/:id', function (req, res) {
+app.put('/api/:id', (req, res) => {
 
-	db.open(function(err, mongoClient){
-		mongoClient.collection('postagens', function(err, collection) {
-			collection.update(
-				{ _id: objectId(req.params.id) },
-				{ $set: {titulo: req.body.titulo}},
+	db.open((err, mongoClient) => {
+		mongoClient.collection('postagens', (err, postagens) => {
+			postagens.update(
+				{_id: objectId(req.params.id)},
+//				{ $set: {titulo: req.body.titulo}},
+				{$set: req.body},
 				{},
-				function(err, records) {
-					if(err) {
+				(err, records) => {
+					if (err) {
 						res.json(err);
 					} else {
 						res.json(records);
@@ -101,14 +157,14 @@ app.put('/api/:id', function (req, res) {
 
 });
 
-app.delete('/api/:id', function (req, res) {
+app.delete('/api/:id', (req, res) => {
 
-	db.open(function(err, mongoClient){
-		mongoClient.collection('postagens', function(err, collection) {
-			collection.remove(
-				{ _id: objectId(req.params.id) },
-				function(err, records) {
-					if(err) {
+	db.open((err, mongoClient) => {
+		mongoClient.collection('postagens', (err, postagens) => {
+			postagens.remove(
+				{_id: objectId(req.params.id)},
+				(err, records) => {
+					if (err) {
 						res.json(err);
 					} else {
 						res.json(records);
@@ -119,4 +175,18 @@ app.delete('/api/:id', function (req, res) {
 		});
 	});
 
+});
+
+/* middleware que configura páginas de status */
+app.use((req, res, next) => {
+	res.status(404).send('Página não encontrada!');
+	// res.status(404).render('errors/404');
+	next();
+});
+/* middleware que configura msgs de erro internos */
+app.use((err, req, res, next) => {
+	console.error(err);
+	res.status(500).send('Desculpe! Houve um erro interno na aplicação.!');
+	// res.status(500).render('errors/500', {err: err});
+	next();
 });
